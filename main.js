6242df082630ed04bb53dddca0139b6f27c7ff72
Marta Maksimovic <marta.maksimovic1988@gmail.com>
2023-03-28 21:49:27 +0200

    feat: add string property access

---
+++ main.js
@@ -0,0 +1,9 @@
+//Property Access on Strings
+//The charAt() method returns the character at a specified index (position) in a string
+
+var str = "I am a string";
+str.charAt(3);
+console.log(str.charAt(0));
+
+//ES5 allows property access on strings:
+console.log(str[2]);
