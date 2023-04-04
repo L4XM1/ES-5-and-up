1d0179fb590ce4acbaa7fdc26a482731b20bc972
Marta Maksimovic <marta.maksimovic1988@gmail.com>
2023-04-04 07:35:37 +0200

    feat: add spread operator

--- main.js
+++ main.js
@@ -500,3 +500,14 @@ console.log(t(2, 6));
 const r = (x, y) => {
   return x * y;
 };
+
+//The Spread (...) Operator
+//expands an iterable into more elements
+const q1 = ["Jan", "Feb", "Mar"];
+const q2 = ["Apr", "May", "Jun"];
+const q3 = ["Jul", "Aug", "Sep"];
+const q4 = ["Oct", "Nov", "Dec"];
+
+const year = [...q1, ...q2, ...q3, ...q4];
+
+console.log(year); // ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
